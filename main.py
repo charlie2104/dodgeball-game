a2a76d8fe0b04392f84d6ec0e3a6252f687929ad
charlie bishop <charlie bishop>
2015-11-18 21:03:14 +0000

    mainly finished need to add a leaderboards

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame, sys, random
 
+#initialise pygame
 pygame.init()
 
 #setting width and height
 width  = 800
 height = 600
 
+#allows for fps
 clock = pygame.time.Clock()
-gameover = False
 
 #info for characters
 cx = width/2
@@ -17,17 +18,15 @@ character_size = 32
 left = False
 right = False
 health = 100
-#info for dodgeballs
 
+#info for dodgeballs
 dx = -60
 dy = 20
-dodgeball_speed = 0.5
 dodgeball_size = 50
 dodgeballs = []
 dodgeball_img = pygame.image.load('dodgeball.png')
 dodge_col_x = 1
 dodge_col_y = 2
-num_of_dodgeballs = 5
 
 #colours  R   G   B
 black = (000,000,000)
@@ -36,16 +35,20 @@ red = (255,000,000)
 green = (000,200,000)
 light_green = (000,255,000)
 dark_red = (200,000,000)
+blue = (000,000,200)
+light_blue = (000,000,255)
 
-smallfont = pygame.font.Font("comic.ttf",25)
-medfont = pygame.font.Font("comic.ttf",50)
-largefont = pygame.font.Font("comic.ttf",80)
+#fonts
+smallfont = pygame.font.Font("luckiestGuy.ttf",25)
+medfont = pygame.font.Font("luckiestGuy.ttf",50)
+largefont = pygame.font.Font("luckiestGuy.ttf",100)
 
+#display
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('dodge Ball')
 
 
-def text_object(text,color, size):
+def text_object(text,color, size):  #gives the text to the message_to_screen function for centering
     if size == "small":
         textSurface = smallfont.render(text, True, color)
     elif size == "medium":
@@ -55,16 +58,12 @@ def text_object(text,color, size):
     return textSurface, textSurface.get_rect()
 
 
-def message_to_screen(msg,color, y_displace = 0, size = "small"):
+def message_to_screen(msg,color, y_displace = 0, size = "small"):   #easy way of putting text onto the screen
     textSurf, textRect = text_object(msg, color, size)
     textRect.center = (width/2), (height/2)+y_displace
     screen.blit(textSurf, textRect)
     
-##for i in range (num_of_dodgeballs):
-##    dx = random.randint(0,width - dodgeball_size)
-##    dodgeballs.append([dodgeball_img, dx, dy,4])
-    
-def health_bar():
+def health_bar():   #draws healthbar
     global health
     
     pygame.draw.rect(screen,black,[11,11,health*4+1, 31])
@@ -74,14 +73,14 @@ def health_bar():
 
 
 
-def draw_dodgeballs():
+def draw_dodgeballs():  #draws dodgeballs onto screen
     for dodgeball in dodgeballs:
         screen.blit(dodgeball[0], (dodgeball[1], dodgeball[2]))
 
-def move_dodgeballs():
+def move_dodgeballs():  #makes the dodgeballs move, also contains dodgeball collisions
     global dodgeball_speed, dodgeball,dodge_split, health
     for dodgeball in dodgeballs:
-        dodgeball[2] += random.randint(10,30)
+        dodgeball[2] += random.randint(dodge_min_speed,dodge_max_speed)
         if dodgeball[2] >= 600:
             dx = random.randint(0,width - dodgeball_size)
             dodgeball[1] = dx
@@ -92,14 +91,14 @@ def move_dodgeballs():
                 dx = random.randint(0,width - dodgeball_size)
                 dodgeball[1] = dx
                 dodgeball[2] = -60
-                health -= 5
+                health -= 10
                
 
 
         
         
 
-
+#these 3 functions are for movement
 def move_left():
     global cx
     cx -= speed
@@ -124,29 +123,9 @@ def check_all_events():
             if event.key == pygame.K_RIGHT:
                 right = True
 
-##def game_over():
-##    global gameover, gameExit
-##    while gameover == True:
-##        screen.fill(white)
-##        message_to_screen('you died',red,-25,'large')
-##        message_to_screen('press q to quit',black,40,'small')
-##        pygame.display.update()
-##        for event in pygame.event.get():
-##            if event.type == pygame.QUIT:
-##                pygame.quit()
-##                quit()
-##            if event.type == pygame.KEYDOWN:
-##                if event.key == pygame.K_c:
-##                    health = 100
-##                    gameover = False
-##                    gameloop()
-##                    break
-##                if event.key == pygame.K_q:
-##                    pygame.quit()
-##                    quit()
 
                     
-def game_intro():
+def game_intro():   #front end of the game
     intro = True
     while intro:
         screen.fill(white)
@@ -155,19 +134,20 @@ def game_intro():
 
         mouse = pygame.mouse.get_pos()
 
-        text_start = medfont.render('Start!',True,black)
-        text_quit = medfont.render('Quit',True,black)
+        text_start = medfont.render('Start!',True,white)
+        text_quit = medfont.render('Quit',True,white)
 
 
         pygame.draw.rect(screen,green,[100,400,230,70])
         pygame.draw.rect(screen,dark_red,[470,400,230,70])
-
+#play
         if 100+230 > mouse[0] > 100 and 400+70 > mouse[1] > 400:
             pygame.draw.rect(screen,light_green,[105,405,220,60])
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     intro = False
-                    gameloop()
+                    difficulty_choice()
+#quit
         if 470+230 > mouse[0] > 470 and 400+70 > mouse[1] > 400:
             pygame.draw.rect(screen,red,[475,405,220,60])
             for event in pygame.event.get():
@@ -181,16 +161,120 @@ def game_intro():
                 quit()
 
 
-        screen.blit(text_start, [140,395,230,70])
-        screen.blit(text_quit, [530,395,230,70])
+        screen.blit(text_start, [140,415,230,70])
+        screen.blit(text_quit, [530,415,230,70])
         pygame.display.update()
         clock.tick(15)
     
+def game_over():    #game over screen for death
+    global gameover
+    while gameover == True:
+        mouse = pygame.mouse.get_pos()
 
+        text_replay = medfont.render('replay',True,white)
+        text_quit = medfont.render('Quit',True,white)
+
+
+        pygame.draw.rect(screen,green,[100,400,230,70])
+        pygame.draw.rect(screen,dark_red,[470,400,230,70])
+#restart
+        if 100+230 > mouse[0] > 100 and 400+70 > mouse[1] > 400:
+            pygame.draw.rect(screen,light_green,[105,405,220,60])
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    gameover = False
+                    difficulty_choice()
+#quit
+        if 470+230 > mouse[0] > 470 and 400+70 > mouse[1] > 400:
+            pygame.draw.rect(screen,red,[475,405,220,60])
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pygame.quit()
+                    quit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        screen.blit(text_replay, [135,415])
+        screen.blit(text_quit, [530,415])
+        pygame.display.update()
+
+
+def scoring():  #a scoring system based on time
+    global time, time_displayed
+    if time > 70000000000: #there are 70000000000 in a second
+        time = clock.get_time()
+        time_displayed += 1
+    time += time
+    text_time = smallfont.render('time:' + str(time_displayed),True,black)
+    screen.blit(text_time,[10,570])
+
+def difficulty_choice():    #allows the user to choose a suitable difficulty
+    global num_of_dodgeballs,dodge_min_speed,dodge_max_speed
+    screen.fill(white)
+    difficulty_choice = True
+    while difficulty_choice == True:
+        mouse = pygame.mouse.get_pos()
+
+        message_to_screen('choose your difficulty',black,-250,'medium')
+
+        pygame.draw.rect(screen,green,[290,120,220,90])
+        pygame.draw.rect(screen,blue,[290,285,220,90])
+        pygame.draw.rect(screen,dark_red,[290,450,220,90])
+#easy
+        if 290 + 220 > mouse[0] > 290 and 120 + 90 > mouse[1] > 120:
+            pygame.draw.rect(screen,light_green,[295,125,210,80])
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    num_of_dodgeballs = 4
+                    dodge_min_speed = 10
+                    dodge_max_speed = 15
+                    difficulty_choice = False
+                    gameloop()
+#medium
+        if 290 + 220 > mouse[0] > 290 and 285 + 90 > mouse[1] > 285:
+            pygame.draw.rect(screen,light_blue,[295,290,210,80])
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    num_of_dodgeballs = 6
+                    dodge_min_speed = 15
+                    dodge_max_speed = 25
+                    difficulty_choice = False
+                    gameloop()
+#hard
+        if 290 + 220 > mouse[0] > 290 and 450 + 90 > mouse[1] > 450:
+            pygame.draw.rect(screen,red,[295,455,210,80])
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    num_of_dodgeballs = 8
+                    dodge_min_speed = 25
+                    dodge_max_speed = 35
+                    difficulty_choice = False
+                    gameloop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        text_easy = medfont.render('easy',True,white)
+        text_medium = medfont.render('medium',True,white)
+        text_hard = medfont.render('hard',True,white)
+        screen.blit(text_easy,[340,149])
+        screen.blit(text_medium,[305,313])
+        screen.blit(text_hard,[340,477])
+        
+        pygame.display.update()
+        clock.tick(15)
     
-def gameloop():
-    global cx, cy, speed, left, right, dx, character_size, dy, dodge_col_x, dodge_col_y,dodge_split,gameover,health,dodgeballs
-    gameExit =  False
+def gameloop():   #the main function, cointains all of the game
+    global cx, cy, speed, left, right, dx, character_size, dy, dodge_col_x, dodge_col_y,gameover,health,dodgeballs,time,time_displayed
+#time variables
+    time = clock.get_time()
+    time_displayed = 0
+
+#used to detect if death has occured
+    gameover = False
 
 #info for characters
     cx = width/2
@@ -200,46 +284,34 @@ def gameloop():
     left = False
     right = False
     health = 100
-#info for dodgeballs
 
+#info for dodgeballs
     dx = -60
     dy = 20
-    dodgeball_speed = 0.5
-    dodgeball_size = 50
     dodgeballs = []
     dodgeball_img = pygame.image.load('dodgeball.png')
     dodge_col_x = 1
     dodge_col_y = 2
-    num_of_dodgeballs = 5
 
+
+#making list of dx and dy for each dodgeball
     for i in range (num_of_dodgeballs):
         dx = random.randint(0,width - dodgeball_size)
         dodgeballs.append([dodgeball_img, dx, dy,4])
 
-    while not gameExit:
+#main game loop starts
+    while True:
+        screen.fill(white)
+        
+#handles death
         if gameover == True:
             screen.fill(white)
             message_to_screen('you died',red,-25,'large')
-            message_to_screen('press q to quit or c to play again',black,40,'small')
+            game_over()
             pygame.display.update()
-            while gameover == True:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        pygame.quit()
-                        quit()
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_c:
-                            health = 100
-                            gameover = False
-                            gameloop()
-                            break
-                        if event.key == pygame.K_q:
-                            pygame.quit()
-                            quit()
-        screen.fill(white)
-        check_all_events()
-                  
-    
+
+#movement
+        check_all_events()             
         if cx < width - character_size:
             if right:
                 move_right()
@@ -248,41 +320,19 @@ def gameloop():
             if left:
                 move_left()
 
-
-            
-        dodge_split = str(dodgeballs).split(",")
+#drawing stuff and main game functions
         pygame.draw.rect(screen, black, [cx, cy, character_size, character_size])
         draw_dodgeballs()
         move_dodgeballs()
         health_bar()
+        scoring()
+#decides if death has happened
         if health <= 0:
             gameover = True
-        #dodge_split = str(dodgeballs).split(",")
-
-
-##        the old hard coded split colissions  
-##        if cx > int(dodge_split[1]) and cx < int(dodge_split[1]) + dodgeball_size or cx + character_size > int(dodge_split[1]) and cx + character_size < int(dodge_split[1]) + dodgeball_size:
-##            if cy > int(dodge_split[2]) and cy < int(dodge_split[2]) + dodgeball_size or cy + character_size > int(dodge_split[2]) and cy + character_size < int(dodge_split[2]) + dodgeball_size:
-##                dx = random.randint(0,width - dodgeball_size)
-##                dodgeball[1] = dx
-##
-##                dodgeball[2] = -60
-##        if cx > int(dodge_split[5]) and cx < int(dodge_split[5]) + dodgeball_size or cx + character_size > int(dodge_split[5]) and cx + character_size < int(dodge_split[5]) + dodgeball_size:
-##            if cy > int(dodge_split[6]) and cy < int(dodge_split[6]) + dodgeball_size or cy + character_size > int(dodge_split[6]) and cy + character_size < int(dodge_split[6]) + dodgeball_size:
-##                dx = random.randint(0,width - dodgeball_size)
-##                dodgeball[1] = dx
-##
-##                dodgeball[2] = -60
-
-
- 
+#screen updates
         pygame.display.update()
         clock.tick(30)
 
-
-
-
-    pygame.quit
-    quit()
-game_intro()    
+#start of game
+game_intro()  
 
